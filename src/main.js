e4b0bc19bb99fd79be49e96f280467947bfd50d6
Jingqi Huang <dicskyowner@gmail.com>
2023-05-06 13:03:25 -0700

    upload wav

--- src/main.js
+++ src/main.js
@@ -10,3 +10,12 @@ let game = new Phaser.Game(config);
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 let keyJUMP, keyQ, keyE;
+
+
+
+
+//Jingqi Huang
+//MAOMAORUN
+//10 Hours
+//Creative Tilt：
+//
